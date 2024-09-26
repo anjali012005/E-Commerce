1dd4774c6dce3d8f66ac7177e6d3334c2d964081
Anjali <anjalidaharwal5@gmail.com>
2024-09-27 00:00:49 +0530

    added footer, newcollections, newsletter, shopcontext etc..

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+// import '../CSS/ShopCategory.css';
 
 const ShopCategory = () => {
+    const { all_product } = useContext(ShopContext);
     return (
-        <div>ShopCategory</div>
+        <div className='shop-category'>
+            k
+        </div>
     )
 }
 
