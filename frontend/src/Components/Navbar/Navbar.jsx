778b219c90f8be71d5fe81ffa03497679866c682
Anjali <anjalidaharwal5@gmail.com>
2024-10-01 12:59:46 +0530

    done with backend part

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -34,7 +34,8 @@ const Navbar = () => {
             </ul>
 
             <div className='nav-login-cart'>
-                <Link to="/login"><button>Login</button></Link>
+                {localStorage.getItem('auth-token') ? <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/') }}>Logout</button> : <Link to="/login"><button>Login</button></Link>}
+
                 <Link to="/cart"><img src={cart} alt="cart" /></Link>
                 <div className='nav-cart-count'>
                     {getTotalCartItems()}
